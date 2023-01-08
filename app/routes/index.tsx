6371d9c4e27f20946c841c0d57ff57085b5fea28
Saalik <saalikl111@gmail.com>
2023-01-07 20:58:34 -0600

    first commit -setup remix, sqlite, prisma

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to GiveGrats</h1>
+      
+    </div>
+  );
+}
---
+++ prisma/migrations/20230108025037_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Gratitude" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL
+);
