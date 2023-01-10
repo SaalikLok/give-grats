6069110209b2daea2b9e381744445f094e54ae3e
Saalik <saalikl111@gmail.com>
2023-01-10 06:55:10 -0600

    feat: basic gratitude submission form

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,31 @@
+import type { ActionFunction } from '@remix-run/node'
+import { Form } from '@remix-run/react'
 import type { ReactElement } from 'react'
+import { createGratitude } from '~/models/gratitude.server'
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+  const grat = formData.get('grat')?.toString()
+  if (grat !== undefined) {
+    await createGratitude(grat)
+  }
+
+  console.log({ grat })
+  return { ok: true }
+}
 
 export default function Index (): ReactElement {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to GiveGrats</h1>
+    <main>
+      <h1 className='title'>GiveGrats</h1>
 
-    </div>
+      <Form method='post'>
+        <fieldset>
+          <textarea name='grat' id='grat' cols={30} rows={10} />
+          <button type='submit'>Give</button>
+        </fieldset>
+      </Form>
+
+    </main>
   )
 }
